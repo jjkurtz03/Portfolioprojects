66b1a632f339f0cb7967961afd0d9b796022b25c
Josh Kurtz <jjkurtz03@gmail.com>
2021-07-06 12:41:13 -0500

    fixed carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,11 @@
 $(function() {
-    $(".carousel").carousel( { interval: 2000} );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
+      $(".left").click(function(){
+        $("#homeCarousel").carousel("prev");
+      });
+      $(".right").click(function(){
+        $("#homeCarousel").carousel("next");
+      });
     });
     $("#loginButton").click(function(){
-    $("#loginModal").modal("show");
-    });
-});
+        $("#loginModal").modal("show");
+  });
