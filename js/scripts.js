3d323c92d0ddf3f34c9b91b65f66af9a933c38bf
Josh Kurtz <jjkurtz03@gmail.com>
2021-07-09 10:20:15 -0500

    final touches of HTML updates

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,15 @@ $(function() {
       $(".right").click(function(){
         $("#homeCarousel").carousel("next");
       });
+      $(".item1").click(function(){
+        $("#homeCarousel").carousel(0);
+      });
+      $(".item2").click(function(){
+        $("#homeCarousel").carousel(1);
+      });
+      $(".item3").click(function(){
+        $("#homeCarousel").carousel(2);
+      });
     });
     $("#loginButton").click(function(){
         $("#loginModal").modal("show");
